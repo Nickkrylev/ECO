128100efbcf41ffc7891d6344e8d71fa49adf4a4
Nickkrylev <mikita.krylev@gmail.com>
2023-10-17 16:16:55 +0300

    update srcipt.sql

--- script.sql
+++ script.sql
@@ -24,31 +24,3 @@ CREATE TABLE pollution (
     FOREIGN KEY (ID_polluter) REFERENCES polluter(ID)
 );
 
--- Insert data into the factory table
-INSERT INTO factory (Name_factory, Adresa, Info) VALUES
-    ('Factory A', 'Address A1', 'Info A1'),
-    ('Factory B', 'Address B1', 'Info B1'),
-    ('Factory C', 'Address C1', 'Info C1');
-
--- Insert data into the polluter table
-INSERT INTO polluter (Name_polluter, GDK, GDK_midle, Dangerous_level) VALUES
-    ('Polluter X', 10.5, 8.2, 3),
-    ('Polluter Y', 7.8, 6.0, 2),
-    ('Polluter Z', 5.5, 4.9, 1);
-
--- Insert data into the pollution table
-INSERT INTO pollution (ID_factory, ID_polluter, Count_pollution, Year_pollution) VALUES
-    (1, 1, 100.5, '2022'),
-    (1, 2, 75.3, '2022'),
-    (2, 1, 90.8, '2022'),
-    (2, 2, 60.2, '2022'),
-    (3, 3, 50.0, '2022');
-SELECT
-    f.Name_factory,
-    p.Name_polluter,
-    po.Count_pollution,
-    po.Year_pollution
-FROM
-    pollution po
-INNER JOIN factory f ON po.ID_factory = f.ID
-INNER JOIN polluter p ON po.ID_polluter = p.ID;
